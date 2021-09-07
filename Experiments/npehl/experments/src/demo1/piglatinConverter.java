c04c76a810ba16927c60d1899cfaf9177588b16e
npehl28 <npehl28@iastate.edu>
2021-09-06 21:09:03 -0500

    Replace piglatinConverter.java with a better test called fonts and stuff

--- Experiments/npehl/experments/src/demo1/piglatinConverter.java
+++ Experiments/npehl/experments/src/demo1/piglatinConverter.java
@@ -1,40 +1,41 @@
 package demo1;
 
-import java.util.Scanner;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
-public class piglatinConverter
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public class FontsandStuffTest extends JComponent
 {
+	private static drawingComponent dc;
 	public static void main(String[] args)
 	{
-		System.out.println("Enter in word to convert to piglatin");
-		Scanner scan = new Scanner(System.in);
-		String word = scan.next();
-		System.out.println(convert(word));
-	}
-	public static String convert(String word)
-	{
-		String vowels = "AEIOUaeuio";
-		for(int i = 0; i<vowels.length(); i++)
-		{
-			if(word.charAt(0) == vowels.charAt(i))
-			{
-				word = word + "way";
-				return word;
-			}
-		}
-		for(int i = 0; i<vowels.length(); i++)
-		{
-			if(word.charAt(1) == vowels.charAt(i))
-			{
-				char first = word.charAt(0);
-				word = word.substring(1);
-				word = word + first + "ay";
-				return word;
-			}
-		}
-		String firstTwo = word.substring(0,2);
-		word = word.substring(2) + firstTwo + "ay";
-		return word;
+		JFrame frame = new JFrame();
+		frame.setSize(500,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 		
+		dc = new drawingComponent();
+		
+		frame.add(dc);
+	}
+	public static class drawingComponent extends JComponent
+	{
+		public void paintComponent(Graphics g)
+		{
+			Graphics2D g2 = (Graphics2D) g;
+			
+			g2.setFont(new Font("serif", Font.BOLD, 42));
+			
+			g2.drawString("This is a test! :)", 40, 40);
+			
+			g2.setFont(new Font("Monospaced", Font.ITALIC, 21));
+			
+			g2.drawString("This is also a test;)", 40, 90);
+			
+		}
 	}
 }
+
