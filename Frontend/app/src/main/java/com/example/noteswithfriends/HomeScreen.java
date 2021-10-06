44c9ef53df7491a4459d7c96a3cfe431584a7cf5
Brian Reiss <bdreiss@iastate.edu>
2021-10-06 15:10:34 -0500

    Login page working bugs fixed

--- Frontend/app/src/main/java/com/example/noteswithfriends/HomeScreen.java
+++ Frontend/app/src/main/java/com/example/noteswithfriends/HomeScreen.java
@@ -2,7 +2,10 @@ package com.example.noteswithfriends;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        TextView userName = (TextView) findViewById(R.id.usersName);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.userNameMessage);
+        userName.setText("Welcome " + message + "!");
     }
 }
--- Frontend/app/src/main/java/com/example/noteswithfriends/MainActivity.java
+++ Frontend/app/src/main/java/com/example/noteswithfriends/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.noteswithfriends;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -19,29 +23,39 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    String userUsername;
+    String userPassword;
+    String userName;
+    public static String userNameMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CheckPasswords("hi");
+        Button next = (Button) findViewById(R.id.CreateAccount);
+        next.setOnClickListener(this::onClick);
+        Button login = (Button) findViewById(R.id.Login);
+        login.setOnClickListener(this::onClickLogin);
+        TextView testVariable = findViewById(R.id.Postman);
+        testVariable.setText(userPassword);
     }
 
-    private void sendGetRequest(){
-        TextView testVariable = findViewById(R.id.Postman);
-        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-        String url = "https://fb3d6f71-3525-460a-824f-546264c15453.mock.pstmn.io/test/";
-        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                testVariable.setText(response.toString());
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                testVariable.setText("Error");
-            }
-        });
-        queue.add(stringRequest);
+    public void onClick(View view){
+        Intent intent = new Intent(this, CreateAccount.class);
+        startActivity(intent);
+    }
+
+    public void onClickLogin(View view){
+        EditText tempUsername = (EditText) findViewById(R.id.UsernameTextBox);
+        userUsername = tempUsername.getText().toString();
+        CheckPasswords(userUsername);
+        EditText enteredPassword = (EditText) findViewById(R.id.PasswordTextBox);
+        GetName(userUsername);
+        if(enteredPassword.getText().toString().equals(userPassword)){
+            Intent intent = new Intent(this, HomeScreen.class);
+            intent.putExtra(userNameMessage, userName);
+            startActivity(intent);
+        }
     }
 
     private void CheckPasswords(String username){
@@ -52,7 +66,40 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(JSONObject response) {
 
-                    testVariable.setText(response.toString());
+                try {
+                    JSONArray stats = response.getJSONArray(username);
+                    JSONObject userInfo = stats.getJSONObject(0);
+                    testVariable.setText(userInfo.getString("password"));
+                    userPassword = userInfo.getString("password");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                testVariable.setText("Error");
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+    private void GetName(String username){
+        TextView testVariable = findViewById(R.id.Postman);
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String url = "coms-309-064.cs.iastate.edu/users";
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+                    JSONArray stats = response.getJSONArray(username);
+                    JSONObject userInfo = stats.getJSONObject(0);
+                    userName = userInfo.getString("name");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
             }
         }, new Response.ErrorListener() {
