150317789fb392aa3fe7e5138621dfaf6a3dca33
Cameron Lettieri <lettieri@iastate.edu>
2021-10-05 23:58:04 -0500

    Changes to user.java so a table will build in mySQL

--- Experiments/clettieri/backendTest/src/main/java/roundTrip/backendTest/Notes/NoteRepository.java
+++ Experiments/clettieri/backendTest/src/main/java/roundTrip/backendTest/Notes/NoteRepository.java
@@ -1,5 +1,15 @@
 package roundTrip.backendTest.Notes;
 
-public class NoteRepository {
+import org.springframework.transaction.annotation.Transactional;
 
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+	public interface NoteRepository extends JpaRepository<Note, Long> 
+	{
+	    
+	    Note findById(int id);
+
+	   @Transactional
+	    void deleteById(int id);
+	}
