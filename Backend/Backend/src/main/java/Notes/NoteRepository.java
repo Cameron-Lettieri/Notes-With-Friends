cff2f8b3c045d141b882566b9131067506ec6e2a
Cameron Lettieri <lettieri@iastate.edu>
2021-10-15 10:57:03 -0500

    fixed proj structure

---
+++ Backend/Backend/src/main/java/Notes/NoteRepository.java
@@ -0,0 +1,16 @@
+package Notes;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+	public interface NoteRepository extends JpaRepository<Note, Long> 
+	{
+	    
+	    Note findById(int id);
+
+	   @Transactional
+	    void deleteById(int id);
+	}
+
--- Backend/roundTrip/src/main/java/demo2/backend/roundTrip/Notes/NoteType.java
+++
@@ -1,6 +0,0 @@
-package demo2.backend.roundTrip.Notes;
-
-public enum NoteType
-{
-	DEFALUT,TABLE,DRAW,CHECK;
-}
